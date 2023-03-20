46d424252538e908ff76ddae14906f0ce8823f1f
Unexp1ainable <samorepka314@gmail.com>
2023-03-20 18:48:32 +0100

    improved background removal

--- xrepka07.py
+++ xrepka07.py
@@ -2,41 +2,10 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 from ellipse import LsqEllipse
-from matplotlib.patches import Ellipse
 from helpers import *
-# path = "data/finalizace - FIB spots/121-0201G manual looks like 1s though/_2022_121-0201 S9251G, CN_images_FIB_Spots_30 keV; 50 nA.png"
-path = "data/0.png"
 
 
-WINDOW_SIZE = 10
-ALLOWED_DEVIATION = 10
-
-
-def processColumns(img: np.ndarray):
-    wc = img.copy().T
-    width = wc.shape[0]
-    for coli, col in enumerate(img):
-        aa = []
-        avg = np.average(col)
-        val = np.sum(col[0:0+WINDOW_SIZE]) / WINDOW_SIZE
-        if (abs(avg - val) < ALLOWED_DEVIATION):
-            print(f"No significant feature expected. {coli}")
-            continue
-
-        currRef = val
-        for i in range(width - WINDOW_SIZE):
-            val = np.sum(col[i:i+WINDOW_SIZE]) / WINDOW_SIZE
-            if abs(currRef-val) > ALLOWED_DEVIATION:
-                print()
-            aa.append(val)
-
-        # plt.cla()
-        # plt.ylim([0, 255])
-        # plt.plot(aa)
-        # plt.show()
-
-
-def extractBackground(img: np.ndarray):
+def furtherEllipse(img: np.ndarray):
     # find background mean and standard deviation
     center = (img.shape[1]//2, img.shape[0]//2)
 
@@ -64,7 +33,7 @@ def extractBackground(img: np.ndarray):
     # further remove noise (values are 1 or 0, if pixel does not have enough neighbours, the value will be rounded to 0)
     plt.imshow(mask)
     plt.show()
-    mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, np.ones((5, 5)))
+    mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, np.ones((7, 7)))
     plt.imshow(mask)
     plt.show()
     mask = cv.medianBlur(mask, 9)
@@ -75,55 +44,45 @@ def extractBackground(img: np.ndarray):
     ffmask = np.zeros((mask.shape[0]+2, mask.shape[1]+2), dtype=np.uint8)
 
     cv.floodFill(mask, ffmask, (0, 0), 1, flags=cv.FLOODFILL_MASK_ONLY)
-    mask = np.invert(ffmask)
-    # cv.floodFill(ffmask[1:-1, 1:-1], mask, center, 1, flags=cv.FLOODFILL_MASK_ONLY)
-    mask = mask[1:-1, 1:-1]
-    sobel = np.abs(cv.Sobel(mask.astype(np.float32), cv.CV_32F, 1, 0))
-    sobel += np.abs(cv.Sobel(mask.astype(np.float32), cv.CV_32F, 0, 1))
+    # mask = np.invert(ffmask)
+    fmask2 = np.zeros_like(ffmask)
+    cv.floodFill(ffmask[1:-1, 1:-1], fmask2, center, 1, flags=cv.FLOODFILL_MASK_ONLY)
+    fmask2 = fmask2[1:-1, 1:-1]
+    sobel = np.abs(cv.Sobel(fmask2.astype(np.float32), cv.CV_32F, 1, 0))
+    sobel += np.abs(cv.Sobel(fmask2.astype(np.float32), cv.CV_32F, 0, 1))
     sobel = sobel != 0
     # plt.imshow(sobel)
     # plt.show()
 
     a, b = np.nonzero(sobel)
-    fitEllipse(b, a, img)
+    params = fitEllipse(b, a, img)
+    elimg = draw_ellipse(img, *params)
+    plt.imshow(elimg)
+    plt.show()
 
 
 def fitEllipse(X1, X2, img):
     X = np.array(list(zip(X1, X2)))
     reg = LsqEllipse().fit(X)
-    center, width, height, phi = reg.as_parameters()
-
-    # print(f'center: {center[0]:.3f}, {center[1]:.3f}')
-    # print(f'width: {width:.3f}')
-    # print(f'height: {height:.3f}')
-    # print(f'phi: {phi:.3f}')
-
-    fig = plt.figure(figsize=(6, 6))
-    ax = plt.subplot()
-    ax.imshow(cv.cvtColor(img, cv.COLOR_GRAY2RGB))
-    ax.axis('equal')
-    # ax.plot(X1, X2, 'ro', zorder=1)
-    ellipse = Ellipse(
-        xy=center, width=2*width, height=2*height, angle=np.rad2deg(phi),
-        edgecolor='b', fc='None', lw=2, label='Fit', zorder=2
-    )
-    ax.add_patch(ellipse)
-
-    plt.xlabel('$X_1$')
-    plt.ylabel('$X_2$')
-
-    plt.legend()
-    plt.show()
+    return reg.as_parameters()
 
 
 if __name__ == "__main__":
-    # for i in range(9):
-    img = cv.imread(path, cv.IMREAD_GRAYSCALE)
-    # img = img[100: 400, 100: 400]
-    # img = cv.GaussianBlur(img, (5, 5), 0)
+    files = []
+    with open("gut.txt") as file:
+        files = file.readlines()
+    for path in files:
+        path = path.strip()
+        # path = f"data/{i}.png"
+        # path = "data/finalizace - FIB spots/121-0319X manual 5s/_2022_121-0319 S8251X, CN_images_FIB_Spots_2uA_spot_only.png"
+        img = load_image(path)
+        # plt.hist(img.flatten(), 256)
+        # plt.show()
+        # img = img[100: 400, 100: 400]
+        img = cv.GaussianBlur(img, (5, 5), 0)
 
-    # processColumns(img)
-    extractBackground(img)
+        # processColumns(img)
+        furtherEllipse(img)
     # plotImageContours(img, "columns")
 
     # img = cv.imread(path, cv.IMREAD_GRAYSCALE)
