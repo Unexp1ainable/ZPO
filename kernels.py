419dd3ccaf41f05b3f8afd00ae0dc1b35dc505b2
Unexp1ainable <samorepka314@gmail.com>
2023-04-30 11:35:54 +0200

    final tweaks

--- kernels.py
+++ kernels.py
@@ -15,7 +15,7 @@ import numpy as np
 import cv2 as cv
 
 
-def get_ellipse_size(width: int) -> Tuple[int,int]:
+def get_ellipse_size(width: int) -> Tuple[int, int]:
     """Calculate height using sin(55) and width
 
     Args:
@@ -39,17 +39,17 @@ def half_empty(width: int, height: Union[int, None] = None):
     """
     if not height:
         width, height = get_ellipse_size(width)
-    kernel = np.zeros((height,width), dtype=np.float32)
-    kernel[height//2:,:] = 2 #make upper half of filter equal to zero
-    filled_region = cv.ellipse(kernel, (width//2,height//2), (width//2,height//2), 0,0,360, 1, thickness=-1)
-    filled_region_mask = np.array(filled_region==1)
+    kernel = np.zeros((height, width), dtype=np.float32)
+    kernel[height//2:, :] = 2  # make upper half of filter equal to zero
+    filled_region = cv.ellipse(kernel, (width//2, height//2), (width//2, height//2), 0, 0, 360, 1, thickness=-1)
+    filled_region_mask = np.array(filled_region == 1)
     kernel[filled_region_mask] = -1
-    kernel[:height//2,:] = 0
-    kernel[kernel==2] = 1
+    kernel[:height//2, :] = 0
+    kernel[kernel == 2] = 1
     return kernel
 
 
-def half_empty_norm(width, height = None):
+def half_empty_norm(width, height=None):
     """Create a normalized kernel with an ellipse, that is 0 in the upper half.
 
     Args:
@@ -60,14 +60,14 @@ def half_empty_norm(width, height = None):
         np.ndarray: Kernel
     """
     kernel = half_empty(width, height)
-    filled_region_mask = kernel==-1
+    filled_region_mask = kernel == -1
     kernel[filled_region_mask] /= np.count_nonzero(filled_region_mask)
     positive_mask = kernel == 1
     kernel[positive_mask] /= np.count_nonzero(positive_mask)
     return kernel
 
 
-def lower20(width, height = None):
+def lower20(width, height=None):
     """Create a normalized kernel with an ellipse, that is 0 in the upper 80% if the kernel.
 
     Args:
@@ -83,7 +83,6 @@ def lower20(width, height = None):
     area = np.pi * width//2 * height//2
     val = 2/(width*height - area)
     kernel = np.full((height, width), val/3, dtype=np.float32)
-    cv.ellipse(kernel, (width//2,height//2), (width//2,height//2), 0,0,180, -2/area, thickness=-1)
-    cv.ellipse(kernel, (width//2,height//2), (width//2,height//2), 0,180,360, -0.5/area, thickness=-1)
-    kernel[:int(height*0.8),:] = 0
+    cv.ellipse(kernel, (width//2, height//2), (width//2, height//2), 0, 0, 180, -2.5/area, thickness=-1)
+    kernel[:int(height*0.75), :] = 0
     return kernel
